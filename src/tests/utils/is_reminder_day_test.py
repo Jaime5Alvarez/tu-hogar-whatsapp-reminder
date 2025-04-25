01a36c0251826708aec603c2918e73466a580236
Jaime <jaime5alvarez5h@gmail.com>
2025-04-25 23:41:08 +0200

    Actualiza las pruebas de is_reminder_day para utilizar la fecha actual y mejorar la cobertura de casos.

--- src/tests/utils/is_reminder_day_test.py
+++ src/tests/utils/is_reminder_day_test.py
@@ -1,7 +1,9 @@
+import datetime
 from src.utils.utils import is_reminder_day
 
 
 def test_is_reminder_day():
-    assert is_reminder_day("25/04/2025") is True
-    assert not is_reminder_day("25/04/2025")
-    assert not is_reminder_day("25/04/2025")
+    today = datetime.datetime.now() + datetime.timedelta(days=1)
+    assert is_reminder_day(today.strftime("%d/%m/%Y")) is True
+    assert not is_reminder_day("22/04/2025")
+
