2976abb6a21b72ea1a749cb21847f772bfdf60af
Jaime <jaime5alvarez5h@gmail.com>
2025-04-26 00:15:12 +0200

    bugfix tests

--- src/tests/utils/is_reminder_day_test.py
+++
@@ -1,9 +0,0 @@
-import datetime
-from src.utils.utils import is_reminder_day
-
-
-def test_is_reminder_day():
-    today = datetime.datetime.now() + datetime.timedelta(days=1)
-    assert is_reminder_day(today.strftime("%d/%m/%Y")) is True
-    assert not is_reminder_day("22/04/2025")
-
---
+++ src/tests/utils/utils_test.py
@@ -0,0 +1,26 @@
+from src.utils.utils import get_phone_number, is_debug_mode
+from unittest import mock
+import os
+
+import datetime
+from src.utils.utils import is_reminder_day
+
+
+def test_is_debug_mode():
+    with mock.patch.dict(os.environ, {"DEBUG_MODE": "false"}):
+        assert is_debug_mode() is False
+
+    with mock.patch.dict(os.environ, {"DEBUG_MODE": "true"}):
+        assert is_debug_mode() is True
+
+
+
+def test_is_reminder_day():
+    today = datetime.datetime.now() + datetime.timedelta(days=1)
+    assert is_reminder_day(today.strftime("%d/%m/%Y")) is True
+    assert not is_reminder_day("22/04/2025")
+
+def test_get_phone_number():
+    assert get_phone_number(["25/04/2025", "9:00", "John Doe", "1234567890"]) == "1234567890"
+    assert get_phone_number(["25/04/2025", "9:00", "John Doe", "123456 7890"]) == "1234567890"
+    assert not get_phone_number(["25/04/2025", "9:00", "John Doe", "1234567890"]) == "3333333333"
